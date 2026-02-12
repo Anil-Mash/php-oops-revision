2618b498b889a3284841bd132c3c172fbc461b90
Anil-Mash <anilchat341.gpt@gmail.com>
2026-02-12 14:40:42 +0530

    Invoke Method

---
+++ invoke_method.php
@@ -0,0 +1,16 @@
+<?php
+class calculation{
+public $a, $b;
+
+public function __construct($a, $b) {
+    $this->a = $a;
+    $this->b = $b;
+}
+public function __invoke() {
+    echo $this->a * $this->b;
+    }
+}
+
+$obj = new calculation(10, 20);
+$obj();
+?>
